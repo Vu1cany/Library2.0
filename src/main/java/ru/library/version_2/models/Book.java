6bc38b84e8a10d32272d1ac585467dbaa20181f2
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2023-05-26 14:23:36 +0300

    Models from Library were copied

---
+++ src/main/java/ru/library/version_2/models/Book.java
@@ -0,0 +1,61 @@
+package ru.library.version_2.models;
+
+import jakarta.validation.constraints.*;
+
+public class Book {
+    private int bookId;
+
+    @NotEmpty(message = "Поле не может быть пустым")
+    @Pattern(regexp = "[А-Я].+", message = "Корректный формат названия: Название")
+    @Size(max = 100, message = "Слишком длинное название")
+    private String name;
+
+    @NotEmpty(message = "Поле не может быть пустым")
+    @Pattern(regexp = "[А-Я][а-я]+ [А-Я][а-я]+", message = "Корректный формат автора: Имя Фамилия")
+    @Size(max = 100, message = "Слишком длинные имя и фамилия")
+    private String author;
+
+    @Min(value = 1, message = "Год создания должен быть > 1")
+    private int year;
+
+    public Book(int id, String name, String author, int year) {
+        this.bookId = id;
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+
+    public Book() {}
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ src/main/java/ru/library/version_2/models/Person.java
@@ -0,0 +1,48 @@
+package ru.library.version_2.models;
+
+import jakarta.validation.constraints.*;
+
+public class Person {
+    private int personId;
+
+    @NotEmpty(message = "Поле не может быть пустым")
+    @Pattern(regexp = "[А-Я][а-я]+ [А-Я][а-я]+ [А-Я][а-я]+",
+            message = "Корректный формат ФИО: Фамилия Имя Отчество")
+    @Size(max = 40, message = "Слишком длинные ФИО")
+    private String name;
+
+    @Min(value = 1900, message = "Год рождения должен быть после 1900")
+    private int birthYear;
+
+    public Person(int id, String name, int birthYear) {
+        this.personId = id;
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    public Person() {}
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+}

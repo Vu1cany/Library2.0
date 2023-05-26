00417ff60b4cadae1370a2da2d6e68d43d08ea5b
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2023-05-26 21:27:03 +0300

    Updated to use JPA

--- src/main/java/ru/library/version_2/models/Person.java
+++ src/main/java/ru/library/version_2/models/Person.java
@@ -1,17 +1,26 @@
 package ru.library.version_2.models;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
+import org.springframework.stereotype.Component;
 
+@Entity
+@Table(name = "person")
 public class Person {
+    @Id
+    @Column(name = "person_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int personId;
 
     @NotEmpty(message = "Поле не может быть пустым")
     @Pattern(regexp = "[А-Я][а-я]+ [А-Я][а-я]+ [А-Я][а-я]+",
             message = "Корректный формат ФИО: Фамилия Имя Отчество")
     @Size(max = 40, message = "Слишком длинные ФИО")
+    @Column(name = "name")
     private String name;
 
     @Min(value = 1900, message = "Год рождения должен быть после 1900")
+    @Column(name = "birth_year")
     private int birthYear;
 
     public Person(int id, String name, int birthYear) {
